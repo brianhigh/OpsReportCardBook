0574fef69e1dd7c44b84272286aba371cd93e7bf
Brian High <bhigh@live.com>
2017-12-27 15:15:14 -0800

    Reversed order of rendering.

--- render.R
+++ render.R
@@ -13,5 +13,5 @@ if (!require("pacman")) {install.packages("pacman", repos = my_repo)}
 pacman::p_load(knitr, rmarkdown)
 
 # Create .md, .html, and .pdf files
-rmarkdown::render("OpsReportCard.Rmd", "pdf_document")
 rmarkdown::render("OpsReportCard.Rmd", "html_document")
+rmarkdown::render("OpsReportCard.Rmd", "pdf_document")
